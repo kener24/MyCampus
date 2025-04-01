21c5a60f5c4a618bcf3669d36a0a6d921aa882a7
LucaRodrigoo <lrbautista2@uth.hn>
2025-04-01 01:50:22 -0600

    Ventana emergente

--- Views/menu.php
+++ Views/menu.php
@@ -35,12 +35,18 @@
 
 
         <div class="nav-icons d-flex align-items-center">
-            <div class="profile-menu ms-2">
-                <a href="perfil.php"><img src="Home/imagen.php" alt="Perfil"><span>Perfil</span></a>
-            </div>
+            <div class="profile-menu ms-2"> 
+                <img src="Home/imagen.php" alt="Perfil">
+                <span>Perfil</span>
+                <!-- Menú desplegable -->
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="perfil.php">Mi perfil</a>
+                    <a class="dropdown-item" href="edit-perfil.php">Editar perfil</a>
+                    <a class="dropdown-item" href="#" onclick="confirmLogout()">Cerrar sesión</a>
+                </div>
+            </div>   
         </div>
     </nav>
 
-</body>
 
 </html>
---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+// filepath: c:\xampp\htdocs\mycampus\Mycampus\logout.php
+session_start(); // Inicia la sesión
+session_unset(); // Limpia todas las variables de sesión
+session_destroy(); // Destruye la sesión
+header("Location: index.php"); // Redirige al usuario a la página de inicio de sesión
+exit;
